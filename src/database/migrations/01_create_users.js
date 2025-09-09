524d4efb3b0356622c7d2a842bd6f663002c62f0
ramonmirandaa-crypto <ramonmirandaa@gmail.com>
2025-09-09 20:59:03 -0300

    Create 01_create_users.js

---
+++ src/database/migrations/01_create_users.js
@@ -0,0 +1,12 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password_hash').notNullable();
+    table.timestamps(true, true);
+  });
+};
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
